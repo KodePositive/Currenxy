a5d459ef2df225fa1765d9c0306b7195f78132a8
KodePositive <eugene.liwei.wok@gmail.com>
2025-01-08 22:41:39 +0800

    - Fix koin issue. - Update DI

--- app/src/main/java/com/geneus/currenxy/di/AppModules.kt
+++ app/src/main/java/com/geneus/currenxy/di/AppModules.kt
@@ -2,21 +2,35 @@ package com.geneus.currenxy.di
 
 import androidx.room.Room
 import com.geneus.currenxy.data.db.AppDatabase
+import com.geneus.currenxy.data.repository.CurrencyRepository
+import com.geneus.currenxy.data.repository.CurrencyRepositoryImpl
 import com.geneus.currenxy.data.service.ApiService
+import com.geneus.currenxy.domain.usecase.ClearCurrenciesUseCase
+import com.geneus.currenxy.domain.usecase.GetCurrenciesUseCase
+import com.geneus.currenxy.domain.usecase.InsertCurrenciesUseCase
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val appModules = module {
-    useCaseModule()
-    viewmodelModule()
-    dbModule()
     networkModule()
+    dbModule()
+    viewmodelModule()
+
+    repoModule()
+    useCaseModule()
 }
 
 private fun Module.useCaseModule() {
+    single { ClearCurrenciesUseCase(get()) }
+    single { GetCurrenciesUseCase(get()) }
+    single { InsertCurrenciesUseCase(get()) }
+}
 
+
+private fun Module.repoModule() {
+    single<CurrencyRepository> { CurrencyRepositoryImpl(get()) }
 }
 
 private fun Module.viewmodelModule() {
@@ -24,13 +38,11 @@ private fun Module.viewmodelModule() {
 }
 
 private fun Module.dbModule() {
-    // Database
-    single { Room.databaseBuilder(get(), AppDatabase::class.java, "app_database").build() }
+    single { Room.databaseBuilder(get(), AppDatabase::class.java, "currenxy_database").build() }
     single { get<AppDatabase>().currencyDao() }
 }
 
 private fun Module.networkModule() {
-    // Network
     single {
         Retrofit.Builder()
             .baseUrl("")
