bc1d89b8c87bfe70b7cad6837136af5a07c8a9b1
KodePositive <eugene.liwei.wok@gmail.com>
2025-01-09 22:00:51 +0800

    - Add update with button. - Add lists A and B json files.

--- app/src/main/java/com/geneus/currenxy/presentation/ui/DemoActivity.kt
+++ app/src/main/java/com/geneus/currenxy/presentation/ui/DemoActivity.kt
@@ -1,21 +1,15 @@
 package com.geneus.currenxy.presentation.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.geneus.currenxy.R
+import com.geneus.currenxy.databinding.ActivityDemoBinding
 
 class DemoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDemoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_demo)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityDemoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
